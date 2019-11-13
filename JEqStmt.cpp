9d8cbac07affa600d3805a60ec8c6a9983fd4768
etoy22 <ethanleider@gmail.com>
2019-11-13 16:45:28 -0500

    Create JEqStmt.cpp

---
+++ JEqStmt.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <regex>
+#include "DeclIntStmt.h"
+#include "Variable.h"
+
+JEqStmt::JEqStmt() {
+   
+};
+
+JEqStmt::~JEqStmt() {
+};
+
+void JEqStmt::compile(std::string instr) {
+
+}
+
+void JEqStmt::run() {
+	// nothing yet
+}
+
+std::string JEqStmt::toString() {
+    std::string output = "About the DeclIntStmt object: <";
+
+
+    return output;
+}
